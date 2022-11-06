3e1d6c1f027373e5c9f3ca491739acda02e37a41
web-Ali <38067660+web-Ali@users.noreply.github.com>
2022-11-06 08:09:55 +0300

    12

--- scripts.js
+++ scripts.js
@@ -1,51 +1,14 @@
 (() => {
-    function bind(nodes, event, handler) {
-        Array.from(nodes).forEach(node => {
-            node.addEventListener(event, handler);
-        });
-    }
-
-    function makeTabs(node) {
-        let selected = node.querySelector('.section__tab_active').dataset.id;
-        const tabs = node.querySelectorAll('.section__tab');
-        const list = Array.from(tabs).map(node => node.dataset.id);
-        const select = node.querySelector('.section__select');
-
-        function selectTab(newId) {
-            const newTab = node.querySelector(`.section__tab[data-id=${newId}]`);
-            const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
-            const oldTab = node.querySelector('.section__tab_active');
-            const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
-
-            selected = newId;
-
-            oldTab.classList.remove('section__tab_active');
-            oldTab.setAttribute('aria-selected', 'false');
-            oldTab.removeAttribute('tabindex');
-            newTab.classList.add('section__tab_active');
-            newTab.setAttribute('aria-selected', 'true');
-            newTab.setAttribute('tabindex', '0');
-            newTab.focus({
-                preventScroll: true
-            });
-
-            oldPanel.classList.add('section__panel_hidden');
-            oldPanel.setAttribute('aria-hidden', 'true');
-            newPanel.classList.remove('section__panel_hidden');
-            newPanel.setAttribute('aria-hidden', 'false');
-
-            select.value = newId;
-        }
-
-        select.addEventListener('input', () => {
-            selectTab(select.value);
-        });
-
-        bind(tabs, 'click', event => {
-            const newId = event.target.dataset.id;
-            selectTab(newId);
-        });
-
+    function makeTabs() {
+        let tabs = document.querySelector('.section__tabs')
+        let content = document.querySelectorAll('.section__panel')
+        let contentWrap = document.querySelector('.section__panel__wrap')
+        tabs.addEventListener( "click", function(e) {
+            e.currentTarget.querySelector('.section__tab_active').classList.remove('section__tab_active')
+            e.target.classList.toggle('section__tab_active')
+            contentWrap.querySelector( ".section__panel:not(.section__panel_hidden)" ).classList.add("section__panel_hidden")
+            content[e.target.getAttribute("data-content")].classList.remove("section__panel_hidden")
+        } )
     }
 
     function makeMenu() {
@@ -56,7 +19,7 @@
         });
     }
     document.addEventListener('DOMContentLoaded', () => {
-        Array.from(document.querySelectorAll('.main__devices')).forEach(makeTabs);
+        makeTabs()
         makeMenu()
     });
 })();
