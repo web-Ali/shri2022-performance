8a5fcf395c48d2ebaa35b39ebe3758c34c91d3a7
web-Ali <38067660+web-Ali@users.noreply.github.com>
2022-11-06 09:11:23 +0300

    15

--- scripts.js
+++ scripts.js
@@ -1,20 +1,83 @@
 (() => {
-    function makeTabs() {
-        let tabs = document.querySelector('.section__tabs')
-        let content = document.querySelectorAll('.section__panel')
-        let contentWrap = document.querySelector('.section__panel__wrap')
-        let tabSelection = document.querySelector('.section__select')
-        tabSelection.addEventListener("change", function (){
-            contentWrap.querySelector( ".section__panel:not(.section__panel_hidden)" ).classList.add("section__panel_hidden")
-            content[tabSelection.options.selectedIndex].classList.remove("section__panel_hidden")
+    function bind(nodes, event, handler) {
+        Array.from(nodes).forEach(node => {
+            node.addEventListener(event, handler);
         });
-        tabs.addEventListener( "click", function(e) {
-            e.currentTarget.querySelector('.section__tab_active').classList.remove('section__tab_active')
-            e.target.classList.toggle('section__tab_active')
-            tabSelection.options.selectedIndex = e.target.getAttribute("data-id")
-            contentWrap.querySelector( ".section__panel:not(.section__panel_hidden)" ).classList.add("section__panel_hidden")
-            content[e.target.getAttribute("data-id")].classList.remove("section__panel_hidden")
-        } )
+    }
+
+    function makeTabs() {
+        let node = document.querySelector('.main__devices')
+        let selected = node.querySelector('.section__tab_active').dataset.id;
+        const tabs = node.querySelectorAll('.section__tab');
+        const list = Array.from(tabs).map(node => node.dataset.id);
+        const select = node.querySelector('.section__select');
+
+        function selectTab(newId) {
+            const newTab = node.querySelector(`.section__tab[data-id=${newId}]`);
+            const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
+            const oldTab = node.querySelector('.section__tab_active');
+            const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
+
+            selected = newId;
+
+            oldTab.classList.remove('section__tab_active');
+            oldTab.setAttribute('aria-selected', 'false');
+            oldTab.removeAttribute('tabindex');
+            newTab.classList.add('section__tab_active');
+            newTab.setAttribute('aria-selected', 'true');
+            newTab.setAttribute('tabindex', '0');
+            newTab.focus({
+                preventScroll: true
+            });
+
+            oldPanel.classList.add('section__panel_hidden');
+            oldPanel.setAttribute('aria-hidden', 'true');
+            newPanel.classList.remove('section__panel_hidden');
+            newPanel.setAttribute('aria-hidden', 'false');
+
+            select.value = newId;
+        }
+
+        select.addEventListener('input', () => {
+            selectTab(select.value);
+        });
+
+        bind(tabs, 'click', event => {
+            const newId = event.target.dataset.id;
+            selectTab(newId);
+        });
+
+        bind(tabs, 'keydown', event => {
+            if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
+                return;
+            }
+
+            let index = list.indexOf(selected);
+            if (event.which === 37) {
+                // left
+                --index;
+            } else if (event.which === 39) {
+                // right
+                ++index;
+            } else if (event.which === 36) {
+                // home
+                index = 0;
+            } else if (event.which === 35) {
+                // end
+                index = list.length - 1;
+            } else {
+                return;
+            }
+
+            if (index >= list.length) {
+                index = 0;
+            } else if (index < 0) {
+                index = list.length - 1;
+            }
+
+            selectTab(list[index]);
+            event.preventDefault();
+        });
     }
 
     function makeMenu() {
